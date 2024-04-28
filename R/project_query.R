216b6f43f8f7467f71c87d45bec5a8aeff91a8e8
Jussi Kupari <jkupari@KI-CND1457CS2.user.ki.se>
2024-04-28 13:38:13 +0200

    Fix bug to work with Seurat.v5

--- R/project_query.R
+++ R/project_query.R
@@ -84,7 +84,7 @@ if(alignment){
   
   
   
-  new_data <- GetAssayData(new, "data")[shared_features,]
+  new_data <- GetAssayData(new, slot = "data")[shared_features,]
   means <- spmodel@scaling$means
   stdevs  <- spmodel@scaling$stdevs
   new_data <- Matrix::t(new_data)
