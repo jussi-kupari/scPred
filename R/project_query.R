89dcdb7aaf98cf7c040c406d6de8f0ba3f220bf1
Jussi Kupari <jkupari@KI-CND1457CS2.user.ki.se>
2026-01-28 15:40:24 +0100

    Make GetAssayData() use 'layer' instead of 'slot'

--- R/project_query.R
+++ R/project_query.R
@@ -84,7 +84,8 @@ if(alignment){
   
   
   
-  new_data <- GetAssayData(new, slot = "data")[shared_features,]
+  #new_data <- GetAssayData(new, slot = "data")[shared_features,]
+  new_data <- GetAssayData(new, layer = "data")[shared_features,]
   means <- spmodel@scaling$means
   stdevs  <- spmodel@scaling$stdevs
   new_data <- Matrix::t(new_data)
